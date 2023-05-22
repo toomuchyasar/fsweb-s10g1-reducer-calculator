f3f084893f260db471228f5a9f24d4ce3989dcc6
toomuchyasar <yasarmuslu93@outlook.com>
2023-05-22 22:16:31 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
-
+import React, { useReducer } from 'react';
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
+import reducer from './reducers';
+import { initialState } from './reducers';
+import { applyNumber } from './actions';
+import { changeOperation } from './actions';
+import { clearDisplay } from './actions';
+
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+  
+  
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -14,10 +24,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
 
-            <TotalDisplay value={0} />
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation} </span>
+              <span id="memory"><b>Memory:</b> {state.memory} </span>
             </div>
 
             <div className="row">
@@ -27,31 +37,31 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
-              <CalcButton value={2} />
-              <CalcButton value={3} />
+              <CalcButton value={1} onClick={(e) => dispatch(applyNumber(e.target.value))}/>
+              <CalcButton value={2} onClick={(e) => dispatch(applyNumber(e.target.value))}/>
+              <CalcButton value={3} onClick={(e) => dispatch(applyNumber(e.target.value))}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton value={4} onClick={(e) => dispatch(applyNumber(e.target.value))}/>
+              <CalcButton value={5} onClick={(e) => dispatch(applyNumber(e.target.value))}/>
+              <CalcButton value={6} onClick={(e) => dispatch(applyNumber(e.target.value))}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7} />
-              <CalcButton value={8} />
-              <CalcButton value={9} />
+              <CalcButton value={7} onClick={(e) => dispatch(applyNumber(e.target.value))}/>
+              <CalcButton value={8} onClick={(e) => dispatch(applyNumber(e.target.value))}/>
+              <CalcButton value={9} onClick={(e) => dispatch(applyNumber(e.target.value))}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"} />
-              <CalcButton value={"*"} />
-              <CalcButton value={"-"} />
+              <CalcButton value={"+"} onClick={(e) => dispatch(changeOperation(e.target.value))}/>
+              <CalcButton value={"*"} onClick={(e) => dispatch(changeOperation(e.target.value))}/>
+              <CalcButton value={"-"} onClick={(e) => dispatch(changeOperation(e.target.value))}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"} />
+              <CalcButton value={"CE"} onClick={(e) => dispatch(clearDisplay(e.target.value))}/>
             </div>
 
           </form>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY } from './../actions';
 
 export const initialState = {
-  total: 100,
-  operation: "*",
-  memory: 100
+  total: 5,
+  operation: "+",
+  memory: 5,
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -38,6 +38,13 @@ const reducer = (state, action) => {
         ...state,
         operation: action.payload
       });
+
+    case (CLEAR_DISPLAY):
+      return ({
+        ...state,
+        total: 0
+      });
+
 
     default:
       return state;
